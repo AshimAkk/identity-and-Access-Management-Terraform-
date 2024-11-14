3ae10245ccc41550b18e27d601f4b46380e9fd53
Ashim <akkiyemba01@gmail.com>
2024-11-14 21:29:40 +1100

    created the layout of the group policies

--- IAM/users.tf
+++ IAM/users.tf
@@ -25,28 +25,100 @@ resource "aws_iam_group" "Operations" {
 }
 
 
-resource "aws_iam_group" "Finance Manager" {
-  name = "Finance Manager"
+resource "aws_iam_group" "Finance-Manager" {
+  name = "Finance-Manager"
   path = "/"
 }
 
 
 resource "aws_iam_group" "Data Analyts" {
-  name = "Data Analyts"
+  name = "Data-Analyts"
   path = "/"
 }
 
 
 
 
+# creating Policies for Developers group 
+
+resource "aws_iam_group_policy" "developer-group-policy" {
+  name  = "developer-group-policy"
+  group = aws_iam_group.Developers.name
+
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_group_policy" "Operations-group-policy" {
+  name  = "Operations-group-policy"
+  group = aws_iam_group.Operations.name
+
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_group_policy" "Finance-manager-group-policy" {
+  name  = "Finance-manager-group-policy"
+  group = aws_iam_group.Finance-Manager.name
+
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
 
 
 
+resource "aws_iam_group_policy" "Data-Analyts-group-policy" {
+  name  = "Data-Analyts-group-policy"
+  group = aws_iam_group.Data-Analyts.name
 
 
-
-
-
-
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
 
 
