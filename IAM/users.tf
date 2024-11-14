a3f9fcf828c047201f49fda74cd03e077a81b668
Ashim <akkiyemba01@gmail.com>
2024-11-14 21:12:04 +1100

    created strong password policy

--- IAM/users.tf
+++ IAM/users.tf
@@ -1,3 +1,16 @@
+# Creating a strong password policy 
+
+resource "aws_iam_account_password_policy" "password-policy" {
+  minimum_password_length        = 10
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = false
+}
+
+
+
 # Creating the Groups for the different users
 
 
