1bdaa2fa3f6b2bbb6136b89704e8b21f40f8abee
Ashim <akkiyemba01@gmail.com>
2024-11-14 21:09:17 +1100

    created the groups for the 4 different teams

---
+++ IAM/users.tf
@@ -0,0 +1,39 @@
+# Creating the Groups for the different users
+
+
+resource "aws_iam_group" "Developers" {
+  name = "Developers"
+  path = "/"
+}
+
+resource "aws_iam_group" "Operations" {
+  name = "Operations"
+  path = "/"
+}
+
+
+resource "aws_iam_group" "Finance Manager" {
+  name = "Finance Manager"
+  path = "/"
+}
+
+
+resource "aws_iam_group" "Data Analyts" {
+  name = "Data Analyts"
+  path = "/"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
