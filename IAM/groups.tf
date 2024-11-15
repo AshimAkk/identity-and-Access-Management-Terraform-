3dc27ebe258bc8960f3640a11048bc210d1f3939
Ashim <akkiyemba01@gmail.com>
2024-11-16 09:18:56 +1100

    created Developers group, Created and added 4 IAM users to the group

--- IAM/groups.tf
+++ IAM/groups.tf
@@ -1,24 +1,47 @@
-# Creating the Groups for the different users
+# Creating a strong password policy 
 
+resource "aws_iam_account_password_policy" "password-policy" {
+  minimum_password_length        = 10
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = false
+
+}
+
+# Creating Developers Group and Users for the Developers group
+
+resource "aws_iam_group_membership" "Developers" {
+  name = "tf-testing-group-membership"
+
+  users = ["Bob1", "Grace1", "Jane1", "Josh1"]
+
+  group = aws_iam_group.Developers.name
+}
 
 resource "aws_iam_group" "Developers" {
   name = "Developers"
   path = "/"
+
 }
 
-resource "aws_iam_group" "Operations" {
-  name = "Operations"
+resource "aws_iam_user" "Bob1" {
+  name = "Bob1"
+  path = "/"
+}
+resource "aws_iam_user" "Grace1" {
+  name = "Grace1"
   path = "/"
 }
 
 
-resource "aws_iam_group" "Finance-Manager" {
-  name = "Finance-Manager"
+resource "aws_iam_user" "Jane1" {
+  name = "Jane1"
   path = "/"
 }
 
-
-resource "aws_iam_group" "Data Analyts" {
-  name = "Data-Analyts"
+resource "aws_iam_user" "Josh1" {
+  name = "Josh1"
   path = "/"
 }
