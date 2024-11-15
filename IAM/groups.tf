8394299e315ec5b3693946bf9e3b65794ae8fbf1
Ashim <akkiyemba01@gmail.com>
2024-11-16 09:31:02 +1100

    successfully created the IAM groups and users for Operations, Finance Manager, Data Analysts

--- IAM/groups.tf
+++ IAM/groups.tf
@@ -13,7 +13,7 @@ resource "aws_iam_account_password_policy" "password-policy" {
 # Creating Developers Group and Users for the Developers group
 
 resource "aws_iam_group_membership" "Developers" {
-  name = "tf-testing-group-membership"
+  name = "Developers"
 
   users = ["Bob1", "Grace1", "Jane1", "Josh1"]
 
@@ -43,5 +43,85 @@ resource "aws_iam_user" "Jane1" {
 
 resource "aws_iam_user" "Josh1" {
   name = "Josh1"
+  path = "/"
+}
+
+
+
+
+# Creating Operations Group and Users for the Operations group
+
+resource "aws_iam_group_membership" "operations" {
+  name = "operations"
+
+  users = ["Luke1", "Nate1"]
+
+  group = aws_iam_group.operations.name
+}
+
+resource "aws_iam_group" "operations" {
+  name = "operations"
+  path = "/"
+
+}
+
+resource "aws_iam_user" "Luke1" {
+  name = "Luke1"
+  path = "/"
+}
+resource "aws_iam_user" "Nate1" {
+  name = "Nate1"
+  path = "/"
+}
+
+# Creating Finance-Manager Group and Users for the Finance-Manager group
+
+resource "aws_iam_group_membership" "Finance-Manager" {
+  name = "Finance-Manager"
+
+  users = ["Nick"]
+
+  group = aws_iam_group.Finance-Manager.name
+}
+
+resource "aws_iam_group" "Finance-Manager" {
+  name = "Finance-Manager"
+  path = "/"
+
+}
+
+resource "aws_iam_user" "Nick1" {
+  name = "Nick1"
+  path = "/"
+}
+
+
+
+# Creating Data-Analyst Group and Users for the Data-Analyst group
+
+resource "aws_iam_group_membership" "Data-Analyst" {
+  name = "Data-Analyst"
+
+  users = ["Sally1", "Tim1", "Tom1"]
+
+  group = aws_iam_group.Data-Analyst.name
+}
+
+resource "aws_iam_group" "Data-Analyst" {
+  name = "Data-Analyst"
+  path = "/"
+
+}
+
+resource "aws_iam_user" "Sally1" {
+  name = "Sally1"
+  path = "/"
+}
+resource "aws_iam_user" "Tim1" {
+  name = "Tim1"
+  path = "/"
+}
+resource "aws_iam_user" "Tom1" {
+  name = "Tom1"
   path = "/"
 }
--- IAM/variables.tf
+++ IAM/variables.tf
@@ -1,5 +1,5 @@
 # variable "iam_users" {
 #     type = list(string)
 #     default = [ "Bob1", "grace1", "Jane1", "Josh1", "Luke1", "Nate1", "Nick1", "Sally1", "Tim1", "Tom1" ]
-  
+
 # }
